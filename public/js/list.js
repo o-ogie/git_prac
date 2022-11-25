58673ee3548d9399f185fe7bdd36ef26ed0584f4
cheol <qjel60@naver.com>
2022-11-25 15:52:06 +0900

    feat:boards 비어있으면 []추가

--- public/js/list.js
+++ public/js/list.js
@@ -1,2 +1,8 @@
 let item = localStorage.getItem("boards")
 
+if (item === null){
+    const array = []
+    const items = JSON.stringify(array)
+    localStorage.setItem("boards", items)
+    item = array
+}
