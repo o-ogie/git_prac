3cac9dc3f62fd6af2f4feed095bbf061e55ed20f
SW <wrld_worthy@gmail.com>
2022-11-25 15:50:06 +0900

    feat: view.js 게시글 내용 불러오기 구현

--- public/js/view.js
+++ public/js/view.js
@@ -3,6 +3,13 @@ const subject = document.querySelector("#subject")
 const writer = document.querySelector("#writer")
 const content = document.querySelector("#content")
 const date = document.querySelector("#date")
-let index = location.search.split('=')[1]
+const index = location.search.split('=')[1]
+const view = document.querySelectorAll("#view>li>span")
+const next = document.querySelector("#next")
+const prev = document.querySelector("#prev")
 
 
+view[0].innerHTML = boards[index].subject
+view[1].innerHTML = boards[index].writer
+view[2].innerHTML = boards[index].date
+view[3].innerHTML = boards[index].content
