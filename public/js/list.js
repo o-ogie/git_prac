1cb61f14bb8d8180d75c70dc72508759859af54c
cheol <qjel60@naver.com>
2022-11-25 16:05:31 +0900

    feat:table 정보입력되게

--- public/js/list.js
+++ public/js/list.js
@@ -6,3 +6,18 @@ if (item === null){
     localStorage.setItem("boards", items)
     item = array
 }
+
+const tbody = document.querySelector("tbody")
+const boards = JSON.parse(item)
+
+function list(list, index){
+    return `<td>${index}}</td>
+    <td>${list.subject}</td>
+    <td>${list.writer}</td>
+    <td>${list.date}</td>
+    <td>${list.hit}</td>`
+}
+
+for(let i = 0; i < boards.length; i++){
+    tbody.innerHTML += list(boards[i], i)
+}
