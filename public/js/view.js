06c63e3c8ccacc974e074802c60928ca38642c97
SW <wrld_worthy@gmail.com>
2022-11-25 15:37:07 +0900

    fix: view.html a태그 링크수정

--- public/js/view.js
+++ public/js/view.js
@@ -1,2 +1,8 @@
 let boards = JSON.parse(localStorage.getItem('boards'))
+const subject = document.querySelector("#subject")
+const writer = document.querySelector("#writer")
+const content = document.querySelector("#content")
+const date = document.querySelector("#date")
+let index = location.search.split('=')[1]
+
 
