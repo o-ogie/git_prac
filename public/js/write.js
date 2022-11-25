489c76ae5bc5024cb005393f26e650c9ce5715d8
KimGunyoung <KimGunyoung2821>
2022-11-25 16:37:33 +0900

    feat : 인덱스값 부여

--- public/js/write.js
+++ public/js/write.js
@@ -12,6 +12,8 @@ class Board{
 }
 
 function submitHandler(e){
+
+    e.preventDefault()
     const subject = e.target.subject.value
     const content = e.target.content.value
     const writer = e.target.writer.value            //input에 있는 value를 가져와서
@@ -25,7 +27,11 @@ function submitHandler(e){
     const item = JSON.stringify(boards) // 만든객체를 string으로 바꾸고
     localStorage.setItem(`boards`, item)
 
+    location.href = `./board/view.html?index=` + index
     
 }
 
 writeFrm.addEventListener(`submit`, submitHandler)
+
+
+// [{},{},{}]     ?index=0 1 2 
