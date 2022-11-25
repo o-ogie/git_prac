10e5aa4ded68f48473a615e855591b04687a6368
SW <wrld_worthy@gmail.com>
2022-11-25 15:56:50 +0900

    feat: view.js next, prev 구현

--- public/js/view.js
+++ public/js/view.js
@@ -3,7 +3,7 @@ const subject = document.querySelector("#subject")
 const writer = document.querySelector("#writer")
 const content = document.querySelector("#content")
 const date = document.querySelector("#date")
-const index = location.search.split('=')[1]
+const index = parseInt(location.search.split('=')[1])
 const view = document.querySelectorAll("#view>li>span")
 const next = document.querySelector("#next")
 const prev = document.querySelector("#prev")
@@ -13,3 +13,15 @@ view[0].innerHTML = boards[index].subject
 view[1].innerHTML = boards[index].writer
 view[2].innerHTML = boards[index].date
 view[3].innerHTML = boards[index].content
+
+function viewHandler(e){
+    e.preventDefault()
+    if(e.target.id === "next"){
+        location.href = `./view.html?index=${index+1}`
+    }else if(e.target.id === "prev"){
+        location.href = `./view.html?index=${index-1}`
+    }
+}
+
+next.addEventListener("click",viewHandler)
+prev.addEventListener("click",viewHandler)
